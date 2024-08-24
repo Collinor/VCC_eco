b0d09e30a7b1acb45f49491e12d6ae3423db6774
Collinor <149311686+Collinor@users.noreply.github.com>
2024-08-24 13:56:43 +0800

    Update LogTime

--- VCC_Caption1.lua
+++ VCC_Caption1.lua
@@ -34,36 +34,40 @@ Permissions = {{0, "Admin"}, {1, "User"}, {2, "Guest"}}
 --logio
 top.setTextColor (colors.Black)
 function SystemPrint (string)
+    top.blit (CmdTime, colors.gray, colors.white)
     top.blit ("System ", colors.black, colors.lightGray)
     top.blit (string.."\n", colors.black, colors.white)
-    MSGLog[LogLn] = {0, nil, string}
-    LogLn = LogLn + 1
-
-    return true
-end
-
-function ErrorPrint (string)
-    top.blit (" Error ", colors.black, colors.yellow)
-    top.blit (string.."\n", colors.black, colors.white)
-    MSGLog[LogLn] = {1, nil, string}
+    MSGLog[LogLn] = {0, false, string.."\n"}
     LogLn = LogLn + 1
 
     return true
 end
 
 function WarnPrint (string)
-    top.blit ("Warning", colors.black, colors.orange)
+    top.blit (CmdTime, colors.gray, colors.white)
+    top.blit ("Warning", colors.black, colors.yellow)
     top.blit (string.."\n", colors.black, colors.white)
-    MSGLog[LogLn] = {2, nil, string}
+    MSGLog[LogLn] = {1, false, string.."\n"}
+    LogLn = LogLn + 1
+
+    return true
+end
+
+function ErrorPrint (string)
+    top.blit (CmdTime, colors.yellow, colors.white)
+    top.blit (" Error ", colors.black, colors.orange)
+    top.blit (string.."\n", colors.black, colors.white)
+    MSGLog[LogLn] = {2, false, string.."\n"}
     LogLn = LogLn + 1
 
     return true
 end
 
 function FalsePrint (string)
+    top.blit (CmdTime, colors.orange, colors.white)
     top.blit (" False ", colors.white, colors.red)
     top.blit (string.."\n", colors.black, colors.white)
-    MSGLog[LogLn] = {3, 1, string}
+    MSGLog[LogLn] = {3, true, string.."\n"}
     LogLn = LogLn + 1
     MSG = "False"
 
@@ -95,7 +99,7 @@ while true do
     if MSG == "" then goto continue end     -- 检查是否为空
 
     LogTime = os.date ("%Y/%m/%d %H:%M:%S") -- 记录日志时间
-    CmdTime = os.date ("%H:%M:%S")          -- 记录命令时间
+    CmdTime = os.date ("[%H:%M:%S]")          -- 记录命令时间
     if MSG == "exit" then                   -- 是否退出
         SystemPrint ("Exiting...")
         goto EndWhile
