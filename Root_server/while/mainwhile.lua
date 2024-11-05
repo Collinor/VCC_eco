3fc9ad5f5dbee98981f9ceb263eb6c42d79f6d7f
Collinor <149311686+Collinor@users.noreply.github.com>
2024-11-05 21:55:36 +0800

    2024/11/5

--- Root_server/while/mainwhile.lua
+++ Root_server/while/mainwhile.lua
@@ -1,2 +1,14 @@
 --VCC_eco   mainwhile.lua
 --2024 / 11 / 2     23:02:33     Collinor
+
+
+local MSG = ""
+while true do
+    if MSG == "FALSE" then break end
+    if #MSG < 1 then
+        MSG = string.lower(io.read())
+    end
+
+    
+end
+
