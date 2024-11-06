49cea7b536e84647081b63a979083d5ad9899667
Collinor <149311686+Collinor@users.noreply.github.com>
2024-11-06 21:52:09 +0800

    2024/11/6

--- Root_server/while/mainwhile.lua
+++ Root_server/while/mainwhile.lua
@@ -4,9 +4,19 @@
 
 local MSG = ""
 while true do
-    if MSG == "FALSE" then break end
+    if MSG == "FALSE" then break end        -- 当MSG为FALSE时，退出循环
+
+    local CMD = ""
+
     if #MSG < 1 then
-        MSG = string.lower(io.read())
+        MSG = string.lower(io.read())       -- 读取用户输入并转换为小写
+    end
+
+    for i = 1, #MSG do                      -- 遍历字符串 MSG，找到第一个空格的位置
+        if string.sub(MSG, i, i) == " " then
+            CMD = string.sub(MSG, 1, i - 1) -- 提取第一个单词作为命令
+            break
+        end
     end
 
     
