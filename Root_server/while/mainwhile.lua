cf36b39a1338beff23da99772295bdc56539756f
Collinor <149311686+Collinor@users.noreply.github.com>
2024-11-11 21:56:34 +0800

    2024/11/11

--- Root_server/while/mainwhile.lua
+++ Root_server/while/mainwhile.lua
@@ -12,9 +12,9 @@ while true do
         MSG = string.lower(io.read())       -- 读取用户输入并转换为小写
     end
 
-    for i = 1, #MSG do                      -- 遍历字符串 MSG，找到第一个空格的位置
-        if string.sub(MSG, i, i) == " " then
-            CMD = string.sub(MSG, 1, i - 1) -- 提取第一个单词作为命令
+    for i = 1, #MSG-2 do                      -- 遍历字符串 MSG，找到第一个空格的位置
+        if string.sub(MSG, i+1, i+2) == "||" then
+            CMD = string.sub(MSG, 1, i)       -- 提取第一个单词作为命令
             break
         end
     end
