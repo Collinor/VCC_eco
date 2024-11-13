875f2337de6750dae1b4f825cf3ecf618c2871cd
Collinor <149311686+Collinor@users.noreply.github.com>
2024-11-13 21:56:43 +0800

    2024/11/13

--- Root_server/while/mainwhile.lua
+++ Root_server/while/mainwhile.lua
@@ -4,17 +4,25 @@
 
 local MSG = ""
 while true do
-    if MSG == "FALSE" then break end        -- 当MSG为FALSE时，退出循环
+    if MSG == "FALSE" then break end            -- 当MSG为FALSE时，退出循环
 
     local CMD = ""
+    local tArgs = ""
 
     if #MSG < 1 then
-        MSG = string.lower(io.read())       -- 读取用户输入并转换为小写
+        MSG = string.lower(io.read()).."||"     -- 读取用户输入并转换为小写
     end
 
-    for i = 1, #MSG-2 do                      -- 遍历字符串 MSG，找到第一个空格的位置
+    for i = 1, #MSG-2 do                        -- 遍历字符串 MSG，找到 "||" 的位置
         if string.sub(MSG, i+1, i+2) == "||" then
-            CMD = string.sub(MSG, 1, i)       -- 提取第一个单词作为命令
+            for j = 1, i do
+                if string.sub(MSG, j, j) == " " then
+                    CMD = string.sub(MSG, 1, j-1)
+                    tArgs = string.sub(MSG, j+1, i)
+                    break
+                end
+            end
+            MSG = string.sub(MSG, i+3, #MSG)
             break
         end
     end
