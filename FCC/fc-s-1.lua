8deacc1ee1a8eb4203178295b4afd710c1fcb644
Collinor <149311686+Collinor@users.noreply.github.com>
2024-11-18 21:53:36 +0800

    Create fc-s-1.lua

---
+++ FCC/fc-s-1.lua
@@ -0,0 +1,15 @@
+--VCC_eco   fc-s1.lua
+--2024 / 11 / 18     20:43:04     Collinor
+
+
+
+local state = {{nil, 0, 0, 0},{0, 0, 0, 0},{0, 0, 0, 0}}
+local mode = 0                      -- 0: SS   1: SA
+
+
+
+for true do
+    if mode == 0 then
+        
+    end
+end
