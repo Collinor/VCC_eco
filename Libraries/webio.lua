f8dc473cb3992da077b1f05fc08160c4aa4dfadd
Collinor <149311686+Collinor@users.noreply.github.com>
2024-11-24 21:50:55 +0800

    2024/11/24 (2)

--- Libraries/webio.lua
+++ Libraries/webio.lua
@@ -10,18 +10,20 @@ local modem = nil
 
 local webio = 
 {
-    uid                 = os.getComputerID(),
+    cid                 = os.getComputerID(),
     ulable              = os.getComputerLabel(),
     KeyofMessage        = nil,
     ProtocolofMessage   = nil,
-    RootAddress         = nil
+    RootAddress         = nil,
+    uid                 = nil
 }
 
 
 
 webio.Init = function()
+    -- 从JSON文件中读入设置
     if not fs.exists("VCC_sys/web/init.json") then
-        ErrorPrint ("[WebInit] cannot be executed normally:No init.json")
+        FalatPrint ("[WebInit] cannot be executed normally:No init.json")
         return false
     end
     local file = fs.open("VCC_sys/web/init.json", "r")
@@ -29,75 +31,72 @@ webio.Init = function()
     while true do
         temp[#temp+1] = file.readLine()
         if not temp[#temp] then
-            ErrorPrint ("[WebInit] cannot be executed normally:The JSON file is incomplete")
+            FalatPrint ("[WebInit] cannot be executed normally:The JSON file is incomplete")
             break
         end
     end
     webio.KeyofMessage      = temp[1]
     webio.ProtocolofMessage = temp[2]
     webio.RootAddress       = temp[3]
-
-    modem = peripheral.find("modem") or ErrorPrint("[WebInit] cannot be executed normally:No modem attached")
+    -- 初始化网络
+    if not modem == peripheral.find("modem") then
+        FalatPrint ("[WebInit] cannot be executed normally:No modem attached")
+    end
     if not rednet.isOpen("modem") then
-        ErrorPrint ("[WebInit] cannot be executed normally:Failed to open the rednet")
+        FalatPrint ("[WebInit] cannot be executed normally:Failed to open the rednet")
         return false
     end
-    rednet.send(webio.RootAddress, webio.KeyofMessage, webio.ProtocolofMessage)
-    local id, MSG = rednet.receive(webio.ProtocolofMessage, 4)
-    if MSG == nil then
-        ErrorPrint ("[WebInit] cannot be executed normally:Failed to connect to the Root Server")
+    -- 向根服务器获取uid
+    if not rednet.send(webio.RootAddress, webio.KeyofMessage.."-"..webio.ulable.."-INIT", webio.ProtocolofMessage) then
+        FalatPrint ("[WebInit] cannot be executed normally:Failed to send the INIT message to the Root Server")
+    end
+    for i = 1, 4 do
+        local id, MSG = rednet.receive(webio.ProtocolofMessage, 4)
+        if id == webio.RootAddress then
+            webio.uid = MSG
+            return true
+        end
+    end
+
+    FalatPrint ("[WebInit] cannot be executed normally:Failed to connect to the Root Server")
+    iofeedback ("This Computer will be shut down")
+    return false
+end
+
+
+
+webio.ConnectionCheck = function()
+    if not rednet.isopen("modem") then
+        FalatPrint ("Failed to open the rednet, please check the modem")
+        iofeedback ("This Computer will be shut down")
         return false
     end
 
-    return true
-end
-
-
-
---
---Message
-function MessageExp (uid, string)
-    if not rednet.send (uid, string, PoM) then
-        ErrorPrint ("Failed to connect to Computer uid"..uid)
-        return false
+    rednet.send(webio.RootAddress, webio.KeyofMessage.."-"..webio.ulable.."-CHECK", webio.ProtocolofMessage)
+    for i = 1, 4 do
+        local id, MSG = rednet.receive(webio.ProtocolofMessage, 1)
+        if id == webio.RootAddress then
+            webio.uid = MSG
+            return true
+        end
     end
-    return true
+
+    FalatPrint ("[WebInit] cannot be executed normally:Failed to connect to the Root Server")
+    iofeedback ("This Computer will be shut down")
+    return false
 end
 
 
 
-function HTTPStream (uid, string)
-    return true
-end
-    
-function InitMessage (uid, classify)
-    if not rednet.send (uid, classify, ItC) then
-        ErrorPrint ("Failed to find Computer uid "..uid)
-        return false
+webio.Send = function(gid, MSG)
+    if not rednet.send(webio.RootAddress, webio.KeyofMessage.."-"..webio.ulable.."-"..MSG, webio.ProtocolofMessage) then
+        FalatPrint ("[WebSend] cannot be executed normally:Failed to send the message to the Root Server")
     end
-    return true
-end
-
-
-
-function MapInit ()
-    top.setbackgroundColor (colors.blue)
-    top.clear()
-    top.blit ("", colors.black, colors.red)
-    return true
-end
-
-function SendMessages (uid, string, Key)                        -- "id_to_uid-key-string-EOM"
-    if not rednet.send (uid, id.."_to_"..uid..Key..string.."EOM", PoM) then
-        ErrorPrint ("Failed to connect to Computer uid "..uid)
-        return false
+    while true do
+        local id, ret = rednet.receive(webio.ProtocolofMessage, 0.05)
+        if id == gid then
+            return ret
+        end
     end
-    return true
-end
-
-function ConnectionCheck()
-    if rednet.isopen("modem") then
-        
-    end
-    
+    return nil
 end
